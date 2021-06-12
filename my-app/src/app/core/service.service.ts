1dbad3a30e3e901ba6f9ff1f89024f623458c893
Rajat <dey.rajat33@gmail.com>
2021-06-12 21:55:45 +0530

    Proxy service file added, api call initiated

--- my-app/src/app/core/service.service.ts
+++ my-app/src/app/core/service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  rootURL = '/api';
+
+  getstudentList() {
+    return this.http.get(this.rootURL + '/studentList');
+  }
+
+  addstudent(student: any) {
+    return this.http.post(this.rootURL + '/student', {student});
+  }
+
 }
