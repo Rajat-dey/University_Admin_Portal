191ef00087cff84cf716bbe5f96c0396b8472986
Rajat <dey.rajat33@gmail.com>
2021-06-13 01:03:12 +0530

    Basic UI Structure created, post & Get api added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DisplayBoardComponent } from './modules/display-board/display-board.component';
+import { StudentsComponent } from './modules/students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    DisplayBoardComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/modules/dashboard/dashboard.component.ts
+++ my-app/src/app/modules/dashboard/dashboard.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ServiceService } from '../../core/service.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnDestroy {
 
-  constructor() { }
+  constructor(private serviceService: ServiceService) {}
 
-  ngOnInit(): void {
+  title = 'Admin Portal';
+
+  userForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('',  Validators.required),
+    email: new FormControl('', Validators.required)
+  });
+
+  users: any[] = [];
+  userCount = 0;
+
+  destroy$: Subject<boolean> = new Subject<boolean>();
+
+  onSubmit() {
+    this.serviceService.addstudent(this.userForm.value).pipe(takeUntil(this.destroy$)).subscribe(data => {
+      console.log('message::::', data);
+      this.userCount = this.userCount + 1;
+      console.log(this.userCount);
+      this.userForm.reset();
+    });
   }
 
+  getAllUsers() {
+    this.serviceService.getstudentList().pipe(takeUntil(this.destroy$)).subscribe((users: any) => {
+        this.users = users;
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
+  }
 }
