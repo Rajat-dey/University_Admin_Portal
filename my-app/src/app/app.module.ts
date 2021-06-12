913d0b111de426f26d103b1ce1fe082e622ce24e
Rajat <dey.rajat33@gmail.com>
2021-06-12 21:05:54 +0530

    Material added, header group complete

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { DashboardComponent } from './modules/dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-app/src/app/shared/header/header.component.ts
+++ my-app/src/app/shared/header/header.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title: string = "Admin Portal";
+
+
   constructor() { }
 
   ngOnInit(): void {
