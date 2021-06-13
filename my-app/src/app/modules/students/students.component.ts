bc0bf84b2a8c39a7028665410f01b04a17f28b4c
Rajat <dey.rajat33@gmail.com>
2021-06-14 04:02:25 +0530

    pagination feature added

--- my-app/src/app/modules/students/students.component.ts
+++ my-app/src/app/modules/students/students.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, ViewChild  } from '@angular/core';
+import { Component, OnInit, Input, ViewChild   } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 
@@ -8,35 +8,29 @@ import {MatTableDataSource} from '@angular/material/table';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-
-  CourseList: string[] = ['DSA', 'Python', 'Electronics', 'Software_Design', 'Chemistry', 'Statics'];
-
-  constructor() { }
-
   @Input() users: any;
 
-
-// studentsData : { AcademicYear: string; Contact: number; FullName: string;  Semester: string;   Subjects: string[]; }[];
-
+  CourseList: string[] = ['DSA', 'Python', 'Electronics', 'Software_Design', 'Chemistry', 'Statics'];
+  constructor() { }
  
 displayedColumns: string[] = [ 'FullName', 'Contact', 'AcademicYear', "Semester","Courses"];
-
-
 dataSource = new MatTableDataSource();
 
+  // @ViewChild(MatPaginator, { static: false })
+  // paginator: MatPaginator;
 
-
-  @ViewChild(MatPaginator, { static: true })
-  paginator!: MatPaginator;
 
 ngOnInit() {
-
-  this.dataSource =this.users;
-  this.dataSource.paginator = this.paginator;
- 
-  console.log(this.dataSource.paginator);
+  this.dataSource = new MatTableDataSource(this.users);
 }
 
+private paginator: MatPaginator;
 
-
+@ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+  this.paginator = mp;
+  this.setDataSourceAttributes();
+}
+setDataSourceAttributes() {
+  this.dataSource.paginator = this.paginator;
+}
 }
