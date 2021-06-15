f357e559dd9dacdef241ee890526cce930da00f6
Rajat <dey.rajat33@gmail.com>
2021-06-15 22:56:54 +0530

    mat button added

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,23 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-
 import {MatSelectModule} from '@angular/material/select';
-
-
 import {MatInputModule} from '@angular/material/input';
 import {  MatTableModule } from '@angular/material/table';
 import {  MatPaginatorModule } from '@angular/material/paginator';
 import {  MatButtonModule } from '@angular/material/button'
-
-
-
-
-
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
